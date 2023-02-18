c3c2fc6e47041be5a651ef672d603d80b866572a
Maria Gonzalez <mariagonzalezova@gmail.com>
2023-02-18 20:59:11 +0100

    jhjh

--- ukol_04.py
+++ ukol_04.py
@@ -1,19 +1,18 @@
 # Aplikace pro zasílání SMS zpráv
 
-telefonni_cislo = input("Zadejte telefonní číslo, na které chcete zprávu zaslat: ")
-delka_cisla = len(telefonni_cislo)
-print(delka_cisla)
-
-if delka_cisla == 13:
-    if telefonni_cislo[0:3] == +420:
-        zprava = input("Napiště zprávu, kterou chcete odeslat: ")
+def overeni_cisla(telefonni_cislo):
+    if len(telefonni_cislo) ==9 or (len(telefonni_cislo)==13 and telefonni_cislo[0:4]=="+420"):
+        return True
     else:
-        print("Zadali jste nesprávnou předvolbu")
-elif delka_cisla == 9:
-    zprava = input("Napiště zprávu, kterou chcete odeslat: ")
-else: 
-    print("zadané číslo nesplňuje požadavky (počet znaků musí být 9 nebo 13 s předvolbou)")
-    exit()
-    print("Zadali jste nesprávnou předvolbu")
+        return False
 
-print(f"K zaplacení {((len(zprava)//180))+1*3} Kč")
+def k_zaplaceni(zprava):
+    castka = (((len(zprava)//180))+1)*3
+    return(castka)
+
+telefonni_cislo = input("Zadejte telefonní číslo, na které chcete zprávu zaslat: ")
+if (overeni_cisla(telefonni_cislo)) == False:
+    print("Zadané číslo nemá správný formát.")
+else:
+    zprava = input("Napiště zprávu, kterou chcete odeslat: ")
+    print(f"K zaplacení {k_zaplaceni(zprava)} Kč")
